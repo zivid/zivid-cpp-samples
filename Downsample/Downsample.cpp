e0fb663d018ed2843bd9f40c7f826c6340b592ca
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2019-08-01 16:59:28 +0200

    issues resolved

--- Downsample/Downsample.cpp
+++ Downsample/Downsample.cpp
@@ -8,55 +8,31 @@ Import a ZDF point cloud and downsample it.
 #include <Eigen/Core>
 
 #include <iostream>
-Eigen::MatrixXf sumline(Eigen::MatrixXf&, int, int);
-Eigen::MatrixXf gridsum(Eigen::MatrixXf&, int);
-float myround(float); 
-float isnanmask(float);
+Zivid::PointCloud downsample(const Zivid::PointCloud&, int);
+Eigen::MatrixXi downsampleandround(const Eigen::MatrixXi&, int);
+Eigen::MatrixXf gridsum(const Eigen::MatrixXf&, int);
+Eigen::MatrixXf sumline(const Eigen::MatrixXf&, int);
 float nantozero(float);
-Zivid::PointCloud downsample(Zivid::PointCloud&, int);
+void visualizepointcloud(const Zivid::PointCloud&, Zivid::Application&);
 
 int main()
 {
 	try
 	{
 		Zivid::Application zivid;
-
-		std::string Filename = "Zivid3D.zdf";
-		std::cout << "Reading " << Filename << " point cloud" << std::endl;
-		Zivid::Frame frame = Zivid::Frame(Filename);
+		
+		std::string filename = "Zivid3D.zdf";
+		std::cout << "Reading " << filename << " point cloud" << std::endl;
+		Zivid::Frame frame(filename);
 
 		auto pointCloud = frame.getPointCloud();
-		auto downSamplingFactor = 4;
 
-		Zivid::PointCloud pointCloudDownsampled(pointCloud.height()/downSamplingFactor, pointCloud.width()/downSamplingFactor);
+		auto downsamplingFactor = 1;
 
-		pointCloudDownsampled = downsample(pointCloud, downSamplingFactor);
+		auto pointCloudDownsampled = downsample(pointCloud, downsamplingFactor);
 
-		std::cout << "Setting up visualization" << std::endl;
-		{
-			Zivid::CloudVisualizer vis;
-			zivid.setDefaultComputeDevice(vis.computeDevice());
-
-			std::cout << "Displaying the original point cloud" << std::endl;
-			vis.showMaximized();
-			vis.show(pointCloud);
-			vis.resetToFit();
-
-			std::cout << "Running the visualizer. Blocking until the window closes" << std::endl;
-			vis.run();
-		}
-		{
-			Zivid::CloudVisualizer vis;
-			zivid.setDefaultComputeDevice(vis.computeDevice());
-
-			std::cout << "Displaying the downsampled point cloud" << std::endl;
-			vis.showMaximized();
-			vis.show(pointCloudDownsampled);
-			vis.resetToFit();
-
-			std::cout << "Running the visualizer. Blocking until the window closes" << std::endl;
-			vis.run();
-		}
+		visualizepointcloud(pointCloud,zivid);
+		visualizepointcloud(pointCloudDownsampled,zivid);
 	}
 
 	catch (const std::exception &e)
@@ -66,60 +42,15 @@ int main()
 	}
 }
 
-Eigen::MatrixXf sumline(Eigen::MatrixXf& matrix, int downSamplingFactor)
-{
-	Eigen::MatrixXf reshapedMatrix = matrix.reshaped(downSamplingFactor, (int)matrix.rows() * (int)matrix.cols() / downSamplingFactor);
-	
-	std::function<float(float)> nan_to_zero_wrap = nantozero;
-
-	reshapedMatrix = reshapedMatrix.unaryExpr(nan_to_zero_wrap);
-
-	return reshapedMatrix.colwise().sum().reshaped((int)matrix.rows() / downSamplingFactor, (int)matrix.cols()).transpose();
-}
-
-Eigen::MatrixXf gridsum(Eigen::MatrixXf& r, int downSamplingFactor)
-{
-	return sumline(sumline(r, downSamplingFactor), downSamplingFactor);
-}
-
-float myround(float x)
-{
-	return std::round(x);
-}
-
-float isnanmask(float x)
-{
-	if (std::isnan(x))
-	{
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
-}
-
-float nantozero(float x)
-{
-	if (std::isnan(x))
-	{
-		return 0;
-	}
-	else
-	{
-		return x;
-	}
-}
-
-Zivid::PointCloud downsample(Zivid::PointCloud& pointCloud, int downSamplingFactor)
+Zivid::PointCloud downsample(const Zivid::PointCloud& pointCloud, int downsamplingFactor)
 {
 	/*
 	Function for downsampling a Zivid point cloud. The downsampling factor represents the denominator of a fraction that represents the size of the downsampled point cloud relative to the original point cloud, e.g. 2 - one-half,  3 - one-third, 4 one-quarter, etc.
 	*/
 
-	if ((downSamplingFactor % 2 != 0) || (pointCloud.height() % downSamplingFactor) || (pointCloud.width() % downSamplingFactor))
+	if ((pointCloud.height() % downsamplingFactor) || (pointCloud.width() % downsamplingFactor))
 	{
-		throw std::invalid_argument("Downsampling factor has to have one of the following values: 2, 3, 4, 5, 6.");
+		throw std::invalid_argument("Downsampling factor (" + std::to_string(downsamplingFactor) + ") has to be divisible by the width (" + std::to_string(pointCloud.width()) + ") and height (" + std::to_string(pointCloud.height()) + ") of the input point cloud, e.g. 2, 3, 4, 5, 6.");
 	}
 
 	Eigen::MatrixXf x(pointCloud.height(), pointCloud.width());
@@ -144,39 +75,26 @@ Zivid::PointCloud downsample(Zivid::PointCloud& pointCloud, int downSamplingFact
 		}
 	}
 
-	Eigen::MatrixXf rf = r.template cast<float>();
-	Eigen::MatrixXf gf = g.template cast<float>();
-	Eigen::MatrixXf bf = b.template cast<float>();
+	Eigen::MatrixXi redDownsampled = downsampleandround(r, downsamplingFactor);
+	Eigen::MatrixXi greenDownsampled = downsampleandround(g, downsamplingFactor);
+	Eigen::MatrixXi blueDownsampled = downsampleandround(b, downsamplingFactor);
 
-	std::function<float(float)> round_wrap = myround;
+	std::function<float(float)> is_not_nan_functor = [](float f) {return !std::isnan(f); };
+	std::function<float(float)> nan_to_zero_functor = nantozero;
 
-	Eigen::MatrixXi redDownsampled = ((gridsum(rf, downSamplingFactor) / (downSamplingFactor*downSamplingFactor)).unaryExpr(round_wrap)).template cast<int>();
-	Eigen::MatrixXi greenDownsampled = ((gridsum(gf, downSamplingFactor) / (downSamplingFactor*downSamplingFactor)).unaryExpr(round_wrap)).template cast<int>();
-	Eigen::MatrixXi blueDownsampled = ((gridsum(bf, downSamplingFactor) / (downSamplingFactor*downSamplingFactor)).unaryExpr(round_wrap)).template cast<int>();
+	Eigen::MatrixXf contrastNulled = (z.unaryExpr(is_not_nan_functor)).cwiseProduct(contrast.unaryExpr(nan_to_zero_functor));
 
-	std::function<float(float)> is_nan_mask_wrap = isnanmask;
-	std::function<float(float)> nan_to_zero_wrap = nantozero;
-
-	Eigen::MatrixXf contrastNulled = (z.unaryExpr(is_nan_mask_wrap)).cwiseProduct(contrast.unaryExpr(nan_to_zero_wrap));
-
-	// Contrast downsampling is WIP
-
-	/*auto contrastWeight = gridsum(contrastNulled, downSamplingFactor);
-	Eigen::MatrixXf contrastContrasted = contrast.cwiseProduct(contrastNulled);
-	Eigen::MatrixXf contrastDownsampled = gridsum(contrastContrasted, downSamplingFactor).cwiseQuotient(gridsum(contrastNulled, downSamplingFactor));
-	std::cout << contrast({ 0,1,2,3,4,5,6,7 }, { 0,1,2,3,4,5,6,7 }) << std::endl;
-	std::cout << contrastNulled({ 0,1,2,3,4,5,6,7 }, { 0,1,2,3,4,5,6,7 }) << std::endl;
-	std::cout << contrastContrasted({ 0,1,2,3,4,5,6,7 }, { 0,1,2,3,4,5,6,7 }) << std::endl;
-	std::cout << contrastWeight({ 0,1,2,3,4,5,6,7 }, { 0,1,2,3,4,5,6,7 }) << std::endl;
-	std::cout << contrastDownsampled({ 0,1, }, { 0,1, }) << std::endl;*/
+	auto contrastWeight = gridsum(contrastNulled, downsamplingFactor);
 
 	Eigen::MatrixXf xContrasted = x.cwiseProduct(contrastNulled);
 	Eigen::MatrixXf yContrasted = y.cwiseProduct(contrastNulled);
 	Eigen::MatrixXf zContrasted = z.cwiseProduct(contrastNulled);
+	Eigen::MatrixXf contrastContrasted = contrast.cwiseProduct(contrastNulled);
 
-	Eigen::MatrixXf xDownsampled = gridsum(xContrasted, downSamplingFactor).cwiseQuotient(gridsum(contrastNulled, downSamplingFactor));
-	Eigen::MatrixXf yDownsampled = gridsum(yContrasted, downSamplingFactor).cwiseQuotient(gridsum(contrastNulled, downSamplingFactor));
-	Eigen::MatrixXf zDownsampled = gridsum(zContrasted, downSamplingFactor).cwiseQuotient(gridsum(contrastNulled, downSamplingFactor));
+	Eigen::MatrixXf xDownsampled = gridsum(xContrasted, downsamplingFactor).cwiseQuotient(contrastWeight);
+	Eigen::MatrixXf yDownsampled = gridsum(yContrasted, downsamplingFactor).cwiseQuotient(contrastWeight);
+	Eigen::MatrixXf zDownsampled = gridsum(zContrasted, downsamplingFactor).cwiseQuotient(contrastWeight);
+	Eigen::MatrixXf contrastDownsampled = gridsum(contrastContrasted, downsamplingFactor).cwiseQuotient(contrastWeight);
 
 	Zivid::PointCloud pointCloudDownsampled(redDownsampled.rows(), redDownsampled.cols());
 
@@ -185,7 +103,7 @@ Zivid::PointCloud downsample(Zivid::PointCloud& pointCloud, int downSamplingFact
 		for (int j = 0; j < redDownsampled.cols(); j++)
 		{
 			pointCloudDownsampled(i, j).setRgb(redDownsampled(i, j), greenDownsampled(i, j), blueDownsampled(i, j));
-			//pointCloudDownsampled(i, j).setContrast(contrastDownsampled(i,j));
+			pointCloudDownsampled(i, j).setContrast(contrastDownsampled(i, j));
 			pointCloudDownsampled(i, j).x = xDownsampled(i, j);
 			pointCloudDownsampled(i, j).y = yDownsampled(i, j);
 			pointCloudDownsampled(i, j).z = zDownsampled(i, j);
@@ -194,3 +112,56 @@ Zivid::PointCloud downsample(Zivid::PointCloud& pointCloud, int downSamplingFact
 
 	return pointCloudDownsampled;
 }
+
+Eigen::MatrixXi downsampleandround(const Eigen::MatrixXi& matrixi, int downsamplingFactor)
+{
+	Eigen::MatrixXf matrixf = matrixi.template cast<float>();
+
+	std::function<float(float)> round_wrap = [](float f) {return std::round(f); };
+
+	return ((gridsum(matrixf, downsamplingFactor) / (downsamplingFactor*downsamplingFactor)).unaryExpr(round_wrap)).template cast<int>();
+}
+
+Eigen::MatrixXf gridsum(const Eigen::MatrixXf& r, int downsamplingFactor)
+{
+	return sumline(sumline(r, downsamplingFactor), downsamplingFactor);
+}
+
+Eigen::MatrixXf sumline(const Eigen::MatrixXf& matrix, int downsamplingFactor)
+{
+	Eigen::MatrixXf reshapedMatrix = matrix.reshaped(downsamplingFactor, (int)matrix.rows() * (int)matrix.cols() / downsamplingFactor);
+
+	std::function<float(float)> nan_to_zero_functor = nantozero;
+
+	reshapedMatrix = reshapedMatrix.unaryExpr(nan_to_zero_functor);
+
+	return reshapedMatrix.colwise().sum().reshaped((int)matrix.rows() / downsamplingFactor, (int)matrix.cols()).transpose();
+}
+
+float nantozero(float x)
+{
+	if (std::isnan(x))
+	{
+		return 0;
+	}
+	else
+	{
+		return x;
+	}
+}
+
+void visualizepointcloud(const Zivid::PointCloud& pointCloud, Zivid::Application& zivid)
+{
+	std::cout << "Setting up visualization" << std::endl;	
+	Zivid::CloudVisualizer vis;
+	zivid.setDefaultComputeDevice(vis.computeDevice());
+
+	std::cout << "Displaying the point cloud" << std::endl;
+	vis.showMaximized();
+	vis.show(pointCloud);
+	vis.resetToFit();
+
+	std::cout << "Running the visualizer. Blocking until the window closes" << std::endl;
+	vis.run();
+}
+
