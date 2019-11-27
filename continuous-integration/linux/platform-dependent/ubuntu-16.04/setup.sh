e15d8d9f02e439167988ed22cc5c3a934d25a3a2
Anders Schau Knatten <anders@knatten.org>
2019-11-27 17:25:18 +0100

    Add CI for Ubuntu 16.04
    There's unfortunately a bug in Zivid which causes issues on Clang 3. An
    internal issue has been logged for this.

---
+++ continuous-integration/linux/platform-dependent/ubuntu-16.04/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+function apt-yes {
+    apt-get --assume-yes "$@"
+}
+
+apt-yes update || exit $?
+apt-yes dist-upgrade || exit $?
+
+apt-yes install software-properties-common || exit $?
+add-apt-repository -y ppa:ubuntu-toolchain-r/test || exit $?
+apt-yes update || exit $?
+
+apt-yes install \
+    g++ \
+    clang \
+    cmake \
+    wget \
+    ninja-build \
+    libx11-dev \
+    libgl1-mesa-glx \
+    || exit $?
+
+function install_www_deb {
+    TMP_DIR=$(mktemp --tmpdir --directory install_www_deb-XXXX) || exit $?
+    pushd $TMP_DIR || exit $?
+    wget -q "$@" || exit $?
+    echo "Installing Zivid debian package $1"
+    apt-yes install --fix-broken ./*deb || exit $?
+    popd || exit $?
+    rm -r $TMP_DIR || exit $?
+}
+
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.7.0+a115eaa4-4/u16/zivid-telicam-driver_2.0.0.1-1_amd64.deb || exit $?
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.7.0+a115eaa4-4/u16/zivid_1.7.0+a115eaa4-4_amd64.deb || exit $?
