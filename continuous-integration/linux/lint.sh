f384589cab50bdaea247b9ce84e4142ff3345a53
Anders Schau Knatten <anders@knatten.org>
2020-03-12 11:09:35 +0100

    More useful error message for clang-format errors
    Fixes #75

--- continuous-integration/linux/lint.sh
+++ continuous-integration/linux/lint.sh
@@ -12,10 +12,20 @@ if [ -z "$cppFiles" ]; then
     exit 1
 fi
 
+echo "Checking formatting"
+errorsFound=0
 for fileName in $cppFiles $hFiles; do
     echo $fileName
-    diff $fileName \
-        <(clang-format-8 $fileName) \
-        || exit $?
+    diff $fileName <(clang-format-8 $fileName)
+    if [ $? -ne 0 ]; then
+        let "errorsFound=errorsFound+1"
+    else
+        echo "ok"
+    fi
 done
-echo Success! ["$0"]
+echo
+if [ $errorsFound -ne 0 ]; then
+    echo "ERROR: $errorsFound formatting error(s) found! See the diffs for each file printed above."
+    exit 1
+fi
+echo "All files are properly formatted!" ["$0"]
