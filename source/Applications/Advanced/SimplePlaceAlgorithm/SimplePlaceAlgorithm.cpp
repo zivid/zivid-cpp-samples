5dea2a93a2dd2af82660024d36f254a6b1773238
Torbjorn Sorby <torbjorn.sorby@zivid.com>
2023-06-11 20:56:43 +0200

    Add side view render

--- source/Applications/Advanced/SimplePlaceAlgorithm/SimplePlaceAlgorithm.cpp
+++ source/Applications/Advanced/SimplePlaceAlgorithm/SimplePlaceAlgorithm.cpp
@@ -4,10 +4,13 @@
 //#include <pcl/common/common.h>
 //#include <pcl/io/pcd_io.h>
 //#include <pcl/point_types.h>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iomanip>
+#include <Zivid/Experimental/Calibration.h>
+#include <Zivid/Zivid.h>
 
 //typedef pcl::PointXYZRGB PointT;
 //typedef pcl::PointCloud<PointT> PointCloudT;
@@ -35,12 +38,14 @@ namespace
             + " ms";
     }
 
-    Matrix3Xd crop_xyz_2d(const std::map<std::string, int>& bin, const Matrix3Xd& xyz)
+    Matrix3Xd crop_xyz_2d(const Eigen::Vector2i& topLeft, const Eigen::Vector2i& bottomRight, const Matrix3Xd& xyz)
     {
-        int row_min = bin.at("row_min");
-        int col_min = bin.at("col_min");
-        int rows = bin.at("row_max") - bin.at("row_min");
-        int cols = bin.at("col_max") - bin.at("col_min");
+        int row_min = topLeft[0];
+        int col_min = topLeft[1];
+        int row_max = bottomRight[0];
+        int col_max = bottomRight[1];
+        int rows = row_max - row_min;
+        int cols = col_max - col_min;
         return {
             xyz[0].block(row_min, col_min, rows, cols),
             xyz[1].block(row_min, col_min, rows, cols),
@@ -48,9 +53,15 @@ namespace
         };
     }
 
-    cv::Mat crop_bgr_2d(const std::map<std::string, int>& bin, const cv::Mat& bgr)
+    cv::Mat crop_bgr_2d(const Eigen::Vector2i& topLeft, const Eigen::Vector2i& bottomRight, const cv::Mat& bgr)
     {
-        cv::Rect roi_rect(bin.at("col_min"), bin.at("row_min"), bin.at("col_max") - bin.at("col_min"), bin.at("row_max") - bin.at("row_min"));
+        int row_min = topLeft[0];
+        int col_min = topLeft[1];
+        int row_max = bottomRight[0];
+        int col_max = bottomRight[1];
+        int rows = row_max - row_min;
+        int cols = col_max - col_min;
+        cv::Rect roi_rect(col_min, row_min, cols, rows);
         return bgr(roi_rect);
     }
 
@@ -101,7 +112,7 @@ namespace
         const Eigen::Vector2d bin_extent_mm = max_values - min_values;
         const Eigen::Vector2d mm_per_pixel(bin_extent_mm[1] / xyz_bin[0].rows(), bin_extent_mm[0] / xyz_bin[0].cols());
         std::cout << "mm per pixel: " << mm_per_pixel << std::endl;
-        return { bin_extent_pixel, bin_extent_mm, mm_per_pixel};
+        return { bin_extent_pixel, bin_extent_mm, mm_per_pixel };
     }
 
     ObjectInfo getObjectInfo(const BinInfo& binInfo, const Eigen::Vector2d& object_extent, const double extent_divider_for_search)
@@ -338,7 +349,7 @@ namespace
 
     void print_time(const std::string& text, const Duration& duration)
     {
-        std::cout << std::left << std::setfill(' ') << std::setw(50) << text << " - Execution time: " << formatDuration(duration) << std::endl;
+        std::cout << "* TIME TO " << std::left << std::setfill(' ') << std::setw(50) << text << " - Execution time: " << formatDuration(duration) << std::endl;
     }
 
     template<typename Function, typename... Args>
@@ -411,12 +422,156 @@ namespace
         }
         return std::make_tuple(xyz, full_bgr);
     }
+
+    open3d::geometry::PointCloud crop3D(open3d::geometry::PointCloud& pointCloud, const Eigen::Vector2i& topLeft, const Eigen::Vector2i& bottomRight)
+    {
+        // Define the bounding box coordinates
+        double min_x = 0.0;   // Minimum X coordinate of the bounding box
+        double max_x = 1.0;   // Maximum X coordinate of the bounding box
+        double min_y = 0.0;   // Minimum Y coordinate of the bounding box
+        double max_y = 1.0;   // Maximum Y coordinate of the bounding box
+        double min_z = 0.0;   // Minimum Z coordinate of the bounding box
+        double max_z = 1.0;   // Maximum Z coordinate of the bounding box
+
+        // Create the bounding box
+        open3d::geometry::AxisAlignedBoundingBox bbox({ min_x, min_y, min_z }, { max_x, max_y, max_z });
+
+    }
+
+    open3d::geometry::PointCloud pointCloudFromData(const Matrix3Xd& xyz, const cv::Mat& bgr)
+    {
+        //std::vector< Eigen::Vector3d > points;
+        auto pointCloud = open3d::geometry::PointCloud();
+        pointCloud.points_.resize(xyz[0].rows() * xyz[0].cols());
+        pointCloud.colors_.resize(xyz[0].rows() * xyz[0].cols());
+
+        for (int row = 0; row < xyz[0].rows(); ++row)
+        {
+            for (int col = 0; col < xyz[0].cols(); ++col)
+            {
+                const auto i = col + row * xyz[0].cols();
+                pointCloud.points_[i] = Eigen::Vector3d(
+                    xyz[0](row, col),
+                    xyz[1](row, col),
+                    xyz[2](row, col)
+                );
+                pointCloud.colors_[i] = Eigen::Vector3d(
+                    bgr.at<cv::Vec3b>(row, col)[2] / 255,
+                    bgr.at<cv::Vec3b>(row, col)[1] / 255,
+                    bgr.at<cv::Vec3b>(row, col)[0] / 255
+                );
+            }
+        }
+
+        return pointCloud;
+    }
+
+    struct CameraIntrinsicsCV
+    {
+        cv::Mat distortionCoefficients;
+        cv::Mat cameraMatrix;
+    };
+
+    CameraIntrinsicsCV reformatCameraIntrinsics(const Zivid::CameraIntrinsics& cameraIntrinsics)
+    {
+        cv::Mat distortionCoefficients(1, 5, CV_64FC1, cv::Scalar(0)); // NOLINT(hicpp-signed-bitwise)
+        cv::Mat cameraMatrix(3, 3, CV_64FC1, cv::Scalar(0));           // NOLINT(hicpp-signed-bitwise)
+
+        distortionCoefficients.at<double>(0, 0) = cameraIntrinsics.distortion().k1().value();
+        distortionCoefficients.at<double>(0, 1) = cameraIntrinsics.distortion().k2().value();
+        distortionCoefficients.at<double>(0, 2) = cameraIntrinsics.distortion().p1().value();
+        distortionCoefficients.at<double>(0, 3) = cameraIntrinsics.distortion().p2().value();
+        distortionCoefficients.at<double>(0, 4) = cameraIntrinsics.distortion().k3().value();
+
+        cameraMatrix.at<double>(0, 0) = cameraIntrinsics.cameraMatrix().fx().value();
+        cameraMatrix.at<double>(0, 2) = cameraIntrinsics.cameraMatrix().cx().value();
+        cameraMatrix.at<double>(1, 1) = cameraIntrinsics.cameraMatrix().fy().value();
+        cameraMatrix.at<double>(1, 2) = cameraIntrinsics.cameraMatrix().cy().value();
+        cameraMatrix.at<double>(2, 2) = 1;
+
+        CameraIntrinsicsCV cameraIntrinsicsCV;
+        cameraIntrinsicsCV.distortionCoefficients = distortionCoefficients;
+        cameraIntrinsicsCV.cameraMatrix = cameraMatrix;
+
+        return cameraIntrinsicsCV;
+    }
+
+    Eigen::Vector3d getCenter(const open3d::geometry::PointCloud& pointCloud)
+    {
+        return pointCloud.GetCenter();
+    }
+
+    void rotatePointCloud(open3d::geometry::PointCloud& pointCloud, const Eigen::Matrix3d& rotation, const Eigen::Vector3d& center)
+    {
+        pointCloud.Rotate(rotation, center);
+    }
+
+    void removeHiddenPoints(open3d::geometry::PointCloud& pointCloud)
+    {
+        pointCloud.HiddenPointRemoval(Eigen::Vector3d(0, 0, 0), 100);
+    }
+
+    cv::Mat projectPoints(open3d::geometry::PointCloud& pointCloud, const Zivid::CameraIntrinsics& zividIntrinsics, const int width, const int height)
+    {
+        // Project points to create 2D image
+        std::vector<cv::Point2d> projectedPoints;
+        projectedPoints.reserve(pointCloud.colors_.size());
+        const cv::Vec3d tvec{ 0, 0, 0 };
+        const cv::Vec3d rvec{ 0, 0, 0 };
+        const auto cvIntrinsics = reformatCameraIntrinsics(zividIntrinsics);
+        const cv::Mat inputArray(pointCloud.points_.size(), 1, CV_64FC3, pointCloud.points_.data());
+        cv::projectPoints(inputArray, rvec, tvec, cvIntrinsics.cameraMatrix, cvIntrinsics.distortionCoefficients, projectedPoints);
+
+        cv::Mat projected_bgr(height, width, CV_8UC3, cv::Scalar(10, 10, 0));
+
+        for (int i = 0; i < projectedPoints.size(); ++i)
+        {
+            auto col = projectedPoints[i].x;
+            auto row = projectedPoints[i].y;
+            //if (projected_bgr.at<cv::Vec3b>(row, col) != cv::Vec3b(100, 100, 0))
+            //{
+            //    std::cout << "Overwriting [" << row << ", " << col << "]" << std::endl;
+            //}
+            projected_bgr.at<cv::Vec3b>(row, col) = cv::Vec3b(
+                convertColorToUint8(pointCloud.colors_[i][2]),
+                convertColorToUint8(pointCloud.colors_[i][1]),
+                convertColorToUint8(pointCloud.colors_[i][0])
+            );
+        }
+        return projected_bgr;
+    }
+
+    cv::Mat get2DSideView(open3d::geometry::PointCloud& pointCloud, const std::string& intrinsics_path, const int width, const int height)
+    {
+        const double pi = 3.14159265358979323846;
+
+        const auto center = timeFunction("get 3D center", getCenter, pointCloud);
+        //const auto center = pointCloud.GetCenter();
+
+        const Eigen::Matrix3d rotation = (Eigen::AngleAxisd(-20 * (pi / 180), Eigen::Vector3d::UnitX()) *
+            Eigen::AngleAxisd(-20 * (pi / 180), Eigen::Vector3d::UnitY()) *
+            Eigen::AngleAxisd(10 * (pi / 180), Eigen::Vector3d::UnitZ())).toRotationMatrix();
+
+        timeVoidFunction("rotate point cloud", rotatePointCloud, pointCloud, rotation, center);
+
+        timeVoidFunction("remove hidden points", removeHiddenPoints, pointCloud);
+
+        // Intrinsics
+        const Zivid::CameraIntrinsics zividIntrinsics(intrinsics_path);
+
+        const auto projected_bgr = timeFunction("project points", projectPoints, pointCloud, zividIntrinsics, width, height);
+
+        return projected_bgr;
+    }
 } // namespace
 
 int main()
 {
+    Zivid::Application zivid;
+
     // INPUT
     std::string pcd_path = "C:/Zivid/Temp/Fizyr/checkerboard_in_bin.pcd";
+    std::string intrinsics_path = "C:/Zivid/Temp/Fizyr/estimated_intrinsics.yaml";
     std::cout << "Loading point cloud from " << pcd_path << std::endl;
     // Load the point cloud using Open3D
     //auto pointCloud = open3d::io::CreatePointCloudFromFile(pcd_path);
@@ -442,7 +597,7 @@ int main()
         std::cout << "Expected " << (width * height) << " points, got " << pointCloud.points_.size() << std::endl;
     }
 
-    const std::map<std::string, int> bin{ { "row_min", 236 }, { "col_min", 229 }, { "row_max", 840 }, { "col_max", 1166 } };
+    const std::vector<Eigen::Vector2i> bin{ Eigen::Vector2i{236, 229 }, Eigen::Vector2i{ 840, 1166 } };
     //const Eigen::Vector2d object_extent_mm(40, 40);
     //const int extent_divider_for_search = 4;
     const Eigen::Vector2d object_extent_mm(200, 300);
@@ -454,34 +609,34 @@ int main()
     std::tie(xyz, full_bgr) = placeDataInMatrices(pointCloud, height, width);
 
     // ALGORITHM
-    const auto xyz_cropped = timeFunction("* TIME TO crop xyz", crop_xyz_2d, bin, xyz);
-    const auto cropped_bgr = timeFunction("* TIME TO crop bgr", crop_bgr_2d, bin, full_bgr);
+    const auto xyz_cropped = timeFunction("crop xyz", crop_xyz_2d, bin[0], bin[1], xyz);
+    const auto cropped_bgr = timeFunction("crop bgr", crop_bgr_2d, bin[0], bin[1], full_bgr);
 
     std::cout << "object_extent_mm = " << object_extent_mm << std::endl;
 
-    const auto bin_info = timeFunction("* TIME TO getBinInfo", getBinInfo, xyz_cropped);
-    const auto object_info = timeFunction("* TIME TO getObjectInfo", getObjectInfo, bin_info, object_extent_mm, extent_divider_for_search);
+    const auto bin_info = timeFunction("getBinInfo", getBinInfo, xyz_cropped);
+    const auto object_info = timeFunction("getObjectInfo", getObjectInfo, bin_info, object_extent_mm, extent_divider_for_search);
 
-    const Matrix5Xd placement_options = timeFunction("* TIME TO find_placement_options", find_placement_options, xyz_cropped, object_info);
+    const Matrix5Xd placement_options = timeFunction("find_placement_options", find_placement_options, xyz_cropped, object_info);
     const PlacementData placement_point = timeFunction("choose_placement_point", choose_placement_point, placement_options);
     std::cout << "Selected place point, translation: " << placement_point.xyz << std::endl;
 
     // VISUALIZATION
     //cv::imshow("BGR image full", full_bgr);
     //cv::imshow("BGR image cropped", cropped_bgr);
-    const auto image_with_all_placements = timeFunction("* TIME TO annotate all placements", annotate_placements, cropped_bgr, placement_options, object_info);
+    const auto image_with_all_placements = timeFunction("annotate all placements", annotate_placements, cropped_bgr, placement_options, object_info);
 
     const auto startTimeImageShowAll = std::chrono::high_resolution_clock::now();
     cv::imshow("Annotated - all points", image_with_all_placements);
     const auto endTimeImageShowAll = std::chrono::high_resolution_clock::now();
-    print_time("* TIME TO show image with all annotations", (endTimeImageShowAll - startTimeImageShowAll));
+    print_time("show image with all annotations", (endTimeImageShowAll - startTimeImageShowAll));
 
     const auto startTimeCloneImage = std::chrono::high_resolution_clock::now();
     auto selected_point_bgr = cropped_bgr.clone();
     const auto endTimeClone = std::chrono::high_resolution_clock::now();
-    print_time("* TIME TO clone image", (endTimeClone - startTimeCloneImage));
+    print_time("clone image", (endTimeClone - startTimeCloneImage));
 
-    timeVoidFunction("* TIME TO annotate selected point", annotatePlacePoint,
+    timeVoidFunction("annotate selected point", annotatePlacePoint,
         selected_point_bgr,
         std::vector<double>{
         placement_options[0](placement_point.pixel_row, placement_point.pixel_col),
@@ -496,7 +651,15 @@ int main()
     const auto startTimeImageShowSelected = std::chrono::high_resolution_clock::now();
     cv::imshow("Annotated - selected point", selected_point_bgr);
     const auto endTimeImageShowSelected = std::chrono::high_resolution_clock::now();
-    print_time("* TIME TO show image with selected annotations", (endTimeImageShowSelected - startTimeImageShowSelected));
+    print_time("show image with selected annotations", (endTimeImageShowSelected - startTimeImageShowSelected));
+
+    // VISUALIZE TRANSFORMED 2D VIEW OF POINT CLOUD
+    auto croppedPointCloud = timeFunction("convert cropped point cloud to Open3D", pointCloudFromData, xyz_cropped, cropped_bgr);
+    //auto croppedPointCloud = pointCloudFromData(xyz_cropped, cropped_bgr);
+    //const auto axisAlignedBoundingBox = open3d::geometry::AxisAlignedBoundingBox::CreateFromPoints(croppedPointCloud.points_);
+    //auto croppedPointCloud = crop3D(pointCloud, bin[0], bin[2]);
+    const auto sideView = get2DSideView(croppedPointCloud.RemoveNonFinitePoints(true, true), intrinsics_path, 1228, 1024);
+    cv::imshow("2D side view", sideView);
 
     cv::waitKey(0);
 
