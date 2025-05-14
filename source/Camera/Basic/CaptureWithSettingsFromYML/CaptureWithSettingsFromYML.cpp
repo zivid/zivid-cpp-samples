5bcbafa488945f901460f10786e1c4e27b96658d
builder <customersuccess@zivid.com>
2025-05-14 16:44:39 +0000

    Samples: Automatic updates to public repository
    Remember to do the following:
        1. Ensure that modified/deleted/new files are correct
        2. Make this commit message relevant for the changes
        3. Force push
        4. Delete branch after PR is merged
    
    If this commit is an update from one SDK version to another,
    make sure to create a release tag for previous version.

--- source/Camera/Basic/CaptureWithSettingsFromYML/CaptureWithSettingsFromYML.cpp
+++ source/Camera/Basic/CaptureWithSettingsFromYML/CaptureWithSettingsFromYML.cpp
@@ -1,13 +1,16 @@
 /*
-Capture images and point clouds, with or without color, from the Zivid camera with settings from YML file.
+Capture images and point clouds, with and without color, from the Zivid camera with settings from YML file.
+
+Choose whether to get the image in the linear RGB or the sRGB color space.
 
 The YML files for this sample can be found under the main Zivid sample instructions.
 */
 
 #include <Zivid/Experimental/PointCloudExport.h>
 #include <Zivid/Zivid.h>
-
+#include <clipp.h>
 #include <iostream>
+#include <string>
 
 namespace
 {
@@ -33,46 +36,85 @@ namespace
     }
 } // namespace
 
-int main()
+int main(int argc, char *argv[])
 {
     try
     {
+        bool linearRgb = false;
+        std::string settingsPath;
+        bool showHelp = false;
+
+        auto cli =
+            (clipp::option("-h", "--help").set(showHelp) % "Show help message",
+             clipp::option("--settings-path")
+                 & clipp::value("path", settingsPath) % "Path to the camera settings YML file",
+             clipp::option("--linear-rgb").set(linearRgb) % "Use linear RGB instead of sRGB");
+
+        if(!clipp::parse(argc, argv, cli) || showHelp)
+        {
+            auto fmt = clipp::doc_formatting{}.alternatives_min_split_size(1).surround_labels("\"", "\"");
+            std::cout << "USAGE:" << std::endl;
+            std::cout << clipp::usage_lines(cli, argv[0], fmt) << std::endl;
+            std::cout << "OPTIONS:" << std::endl;
+            std::cout << clipp::documentation(cli) << std::endl;
+            return showHelp ? EXIT_SUCCESS : EXIT_FAILURE;
+        }
+
         Zivid::Application zivid;
 
         std::cout << "Connecting to camera" << std::endl;
         auto camera = zivid.connectCamera();
 
-        std::cout << "Loading settings from file" << std::endl;
-        const auto settingsFile =
-            std::string(ZIVID_SAMPLE_DATA_DIR) + "/Settings/" + settingsFolder(camera) + "/Settings01.yml";
-        const auto settings = Zivid::Settings(settingsFile);
+        std::cout << "Loading settings from file " << std::endl;
+        if(settingsPath.empty())
+        {
+            const auto settingsFile =
+                std::string(ZIVID_SAMPLE_DATA_DIR) + "/Settings/" + settingsFolder(camera) + "/Settings01.yml";
+            settingsPath = settingsFile;
+        }
+
+        const auto settings = Zivid::Settings(settingsPath);
 
         std::cout << "Capturing 2D frame" << std::endl;
         {
             const auto frame2D = camera.capture2D(settings);
 
-            const auto imageSRGB = frame2D.imageSRGB();
-            const auto imageFile = "ImageSRGB.png";
-            std::cout << "Saving 2D color image (sRGB color space) to file: " << imageFile << std::endl;
-            imageSRGB.save(imageFile);
-
-            // More information about linear RGB and sRGB color spaces is available at:
-            // https://support.zivid.com/en/latest/reference-articles/color-spaces-and-output-formats.html#color-spaces
-            // To get linear RGB image, use the following line instead:
-            // const auto imageLinearRGB = frame2D.imageRGBA();
-
             const auto pixelRow = 100;
             const auto pixelCol = 50;
-            const auto pixelSRGB = imageSRGB(pixelRow, pixelCol);
-            std::cout << "Color at pixel (" << pixelRow << "," << pixelCol << "):  R:" << std::to_string(pixelSRGB.r)
-                      << "  G:" << std::to_string(pixelSRGB.g) << "  B:" << std::to_string(pixelSRGB.b)
-                      << "  A:" << std::to_string(pixelSRGB.a) << std::endl;
+
+            if(linearRgb)
+            {
+                const auto imageRGBA = frame2D.imageRGBA();
+                const auto imageFile = "ImageRGBA_linear.png";
+                std::cout << "Saving 2D color image (Linear RGB) to file: " << imageFile << std::endl;
+                imageRGBA.save(imageFile);
+
+                std::cout << "Extracting 2D pixel array" << std::endl;
+                const auto pixelArrayRGBA = imageRGBA(pixelRow, pixelCol);
+                std::cout << "Color at pixel (" << pixelRow << "," << pixelCol
+                          << "):  R:" << std::to_string(pixelArrayRGBA.r) << "  G:" << std::to_string(pixelArrayRGBA.g)
+                          << "  B:" << std::to_string(pixelArrayRGBA.b) << "  A:" << std::to_string(pixelArrayRGBA.a)
+                          << std::endl;
+            }
+            else
+            {
+                const auto imageSRGB = frame2D.imageRGBA_SRGB();
+                const auto imageFile = "ImageRGBA_sRGB.png";
+                std::cout << "Saving 2D color image (sRGB color space) to file: " << imageFile << std::endl;
+                imageSRGB.save(imageFile);
+
+                std::cout << "Extracting 2D pixel array" << std::endl;
+                const auto pixelArraySRGB = imageSRGB(pixelRow, pixelCol);
+                std::cout << "Color at pixel (" << pixelRow << "," << pixelCol
+                          << "):  R:" << std::to_string(pixelArraySRGB.r) << "  G:" << std::to_string(pixelArraySRGB.g)
+                          << "  B:" << std::to_string(pixelArraySRGB.b) << "  A:" << std::to_string(pixelArraySRGB.a)
+                          << std::endl;
+            }
         }
 
         std::cout << "Capturing 3D frame" << std::endl;
         {
             const auto frame3D = camera.capture3D(settings);
-
             const auto dataFile = "Frame3D.zdf";
             std::cout << "Saving frame to file: " << dataFile << std::endl;
             frame3D.save(dataFile);
